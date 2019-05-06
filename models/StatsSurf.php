04fbd21278ddc2f09559b108db85f4f0359089b0
Andrew <andrew.panix@gmail.com>
2019-05-06 14:22:57 +0300

    init

--- models/StatsSurf.php
+++ models/StatsSurf.php
@@ -43,11 +43,11 @@ class StatsSurf extends ActiveRecord {
             array('dt', 'length', 'min' => 8),
             array('tm', 'length', 'min' => 5),
             array('lang', 'length', 'min' => 2),
-                //array('title, seo_alias', 'required'),
+                //array('title, slug', 'required'),
                 // array('category_id', 'numerical', 'integerOnly' => true),
                 // array('date_create, date_update', 'date', 'format' => 'yyyy-MM-dd HH:mm:ss'),
-                // array('title, seo_alias, seo_title, seo_description, seo_keywords', 'length', 'max' => 255),
-                //array('id, user_id, category_id, title, seo_alias, short_text, full_text, seo_title, seo_description, seo_keywords, date_update, date_create', 'safe', 'on' => 'search'),
+                // array('title, slug, seo_title, seo_description, seo_keywords', 'length', 'max' => 255),
+                //array('id, user_id, category_id, title, slug, short_text, full_text, seo_title, seo_description, seo_keywords, date_update, date_create', 'safe', 'on' => 'search'),
         );
     }
 
